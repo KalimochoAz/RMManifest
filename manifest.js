770902f3da0be61c7e45b409b59e588a9b1e5eee
KalimochoAz <calimochoazucarado@gmail.com>
2012-04-21 11:08:57 +0200

    RomManager manifest initial commit

---
+++ manifest.js
@@ -0,0 +1,138 @@
+{
+    "version":1,
+    "homepage": "http://www.cyanogenmod.com/",
+    "donate":"http://bit.ly/DonateBEER",
+    "roms":[{
+        "name":"Cyanogenmod 9 v4.0",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 Version 4.0",
+        "product":null,
+        "modversion":"2140",
+        "developerIdProp":"KalimochoAz",
+        "label":"Cyanogenmod9",
+        "url":"http://developer.clockworkmod.com/downloads/2958/2444/update-cm-9.0.0-RC0-crespo-signed.no.v4.p0.zip",
+        "incremental":2444
+    },
+    {
+        "name":"Cyanogenmod 9 v3.4",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 Version 3.4",
+        "product":"",
+        "modversion":"2130",
+        "developerIdProp":"KalimochoAz",
+        "label":"Cyanogenmod9",
+        "url":"http://developer.clockworkmod.com/downloads/2958/2172/update-cm-9.0.0-RC0-NS-signed.no.v3.p4.zip",
+        "incremental":2172
+    },
+    {
+        "name":"Cyanogenmod 9 v3.0",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 Version 3.0",
+        "product":"",
+        "modversion":"2130",
+        "developerIdProp":"KalimochoAz",
+        "label":"Cyanogenmod9",
+        "url":"http://developer.clockworkmod.com/downloads/2958/2159/update-cm-9.0.0-RC0-NS-signed.no.v3.p0.zip",
+        "incremental":2159
+    },
+    {
+        "name":"Cyanogenmod 9 v2.1",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 Version 2.1",
+        "product":null,
+        "modversion":"2104",
+        "developerIdProp":"KalimochoAz",
+        "label":"Cyanogenmod9",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1963/update-cm-9.0.0-RC0-NS-signed.no.v2.p1.zip",
+        "incremental":1963
+    },
+    {
+        "name":"Cyanogenmod 9 v2.0",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 Version 2",
+        "product":"",
+        "modversion":"2102",
+        "developerIdProp":"KalimochoAz",
+        "label":"Cyanogenmod9",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1959/update-cm-9.0.0-RC0-NS-signed.no.v2.p0.zip",
+        "incremental":1959
+    },
+    {
+        "name":"Cyanogenmod 9 v1.1",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 Version 1 patch 1",
+        "product":"",
+        "modversion":"2100",
+        "developerIdProp":"kalimochoaz",
+        "label":"Cyanogenmod9",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1789/update-cm-9.0.0-RC0-NS-KANG-signed.no.v1.p1.zip",
+        "incremental":1789
+    },
+    {
+        "name":"Cyanogenmod 9 v1",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 Version 1 patch 0",
+        "product":"",
+        "modversion":"2100",
+        "developerIdProp":"kalimochoaz",
+        "label":"Cyanogenmod9",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1779/update-cm-9.0.0-RC0-NS-signed.no.v1.p0.zip",
+        "incremental":1779
+    },
+    {
+        "name":"KERNEL CyanogenMod v1",
+        "device":"maguro",
+        "summary":"Kernel update to 3.0.16",
+        "product":"",
+        "modversion":"bf9d0de4daf592c3fedc13624b80072a",
+        "developerIdProp":null,
+        "label":"GalaxyNexus/Kernel",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1713/kernel.cyano.RELEASE.v1.10.db526ca0.zip",
+        "incremental":1713
+    },
+    {
+        "name":"Cyanogenmod 9 Alfa 20.p1",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 alfa 20 p1",
+        "product":"",
+        "modversion":"d80ddc5af9676e9acf487db17a839976",
+        "developerIdProp":"cyanogenmod",
+        "label":"Cyanogenmod9/Alfa",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1712/cm_crespo-ota-eng.kalimochoaz.a20.p1.zip",
+        "incremental":1712
+    },
+    {
+        "name":"Cyanogenmod 9 Alfa 20",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 alfa 20",
+        "product":"",
+        "modversion":"7233fb2e5e4805055b5029017ea8394b",
+        "developerIdProp":"cyanogenmod",
+        "label":"Cyanogenmod9/Alfa",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1707/cm_crespo-ota-eng.kalimochoaz.a20.zip",
+        "incremental":1707
+    },
+    {
+        "name":"Cyanogenmod 9 Alfa 18",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 alfa 16",
+        "product":"",
+        "modversion":"CyanogenMod-9.0.0-RC0-NS-KANG",
+        "developerIdProp":"cyanogenmod",
+        "label":"Cyanogenmod9/Alfa",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1642/cm_crespo-ota-eng.kalimochoaz.a18.p1.zip",
+        "incremental":1642
+    },
+    {
+        "name":"Cyanogenmod 9 Alfa 16",
+        "device":"crespo",
+        "summary":"ICS Cyanogenmod 9 alfa 16",
+        "product":"",
+        "modversion":"CyanogenMod-9.0.0-RC0-NS-KANG",
+        "developerIdProp":"cyanogenmod",
+        "label":"Cyanogenmod9/Alfa",
+        "url":"http://developer.clockworkmod.com/downloads/2958/1603/cm_crespo-ota-eng.kalimochoaz.a16.zip",
+        "incremental":1603
+    }
+    ]
+}
